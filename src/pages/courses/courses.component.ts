376ff87733c10d341aae94ffd7b59e3fef35adf7
Evgeniya Marchenkova <evgeniova@gmail.com>
2017-11-26 14:39:32 +0300

    project created

---
+++ src/pages/courses/courses.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'courses-list',
+  templateUrl: './courses.component.html',
+  styleUrls: ['./courses.component.scss']
+})
+export class CoursesComponent {
+  title = 'app';
+}
