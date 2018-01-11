85466488f9fd992913767e839b9f1eb823a161b7
Evgeniya_Marchenkova <evgeniya_marchenkova@epam.com>
2018-01-11 16:47:50 +0300

    fix bug for aothorization service

--- src/core/services/authorization.service.ts
+++ src/core/services/authorization.service.ts
@@ -1,16 +1,15 @@
 import { Injectable } from '@angular/core';
-import * as _ from 'lodash';
-import * as moment from 'moment';
-import {Subject} from 'rxjs/Subject';
-import {ReplaySubject} from 'rxjs/ReplaySubject';
-import {Observable} from 'rxjs/Observable';
+import { Subject } from 'rxjs/Subject';
+import { ReplaySubject } from 'rxjs/ReplaySubject';
+import { Observable } from 'rxjs/Observable';
 
 @Injectable()
 export class AuthorizationService {
   isLoginSubject: Subject<string>;
 
   constructor() {
-    this.isLoginSubject = new ReplaySubject(null);
+    this.isLoginSubject = new ReplaySubject(1);
+    this.isLoginSubject.next(this.userName());
   }
 
   login(username, password) {
@@ -27,5 +26,9 @@ export class AuthorizationService {
 
   isLoggedIn(): Observable<string> {
     return this.isLoginSubject.asObservable();
+  }
+
+  private userName(): string {
+    return localStorage.getItem('username');
   }
 }
